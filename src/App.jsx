d7fdcf78061ef7ba7555b045823505bbf329b33e
sourov2612 <sourov15-4161@diu.edu.bd>
2025-01-21 01:56:37 +0600

    basic interaction done by using context

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+import { MovieContext, ThemeContext } from "./context";
+import Page from "./Page";
+
+export default function App() {
+  const [cartData, setCartData] = useState([]);
+  const [darkMode, setDarkMode] = useState(true);
+  return (
+    <>
+      <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+        <MovieContext.Provider value={{ cartData, setCartData }}>
+          <Page />
+        </MovieContext.Provider>
+      </ThemeContext.Provider>
+    </>
+  );
+}
---
+++ src/Header.jsx
@@ -0,0 +1,65 @@
+import { useState, useContext } from "react";
+import Moon from "./assets/icons/moon.svg";
+import Sun from "./assets/icons/sun.svg";
+import Logo from "./assets/logo.svg";
+import Ring from "./assets/ring.svg";
+import ShoppingCart from "./assets/shopping-cart.svg";
+import CartDetails from "./cine/CartDetails";
+import { MovieContext, ThemeContext } from "./context";
+
+export default function Header() {
+  const [showCart, setShowCart] = useState(false);
+  const { cartData } = useContext(MovieContext);
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
+
+  console.log(cartData);
+
+  function handleCartShow() {
+    setShowCart(true);
+  }
+
+  return (
+    <header>
+      {showCart && <CartDetails onClose={() => setShowCart(false)} />}
+      <nav className="container flex items-center justify-between space-x-10 py-6">
+        <a href="index.html">
+          <img src={Logo} width="139" height="26" alt="" />
+        </a>
+
+        <ul className="flex items-center space-x-5">
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={Ring} width="24" height="24" alt="" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+              onClick={() => setDarkMode((darkMode) => !darkMode)}
+            >
+              <img src={darkMode ? Sun : Moon} width="24" height="24" alt="" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+              onClick={handleCartShow}
+            >
+              <img src={ShoppingCart} width="24" height="24" alt="cart" />
+              {cartData.length > 0 && (
+                <span className="rounded-full absolute top-[-12px] left-[28px] bg-[#12CF6F] text-white text-center p-[2px] w-[30px] h-[30px] ">
+                  {cartData.length}
+                </span>
+              )}
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
